cf3c4d522c5066142792fbc75ef276fbb1d90898
Dominik Neise <neised@phys.ethz.ch>
2020-10-06 17:37:41 +0200

    deal with UID issues badly but quickly

--- build_and_test.sh
+++ build_and_test.sh
@@ -2,24 +2,28 @@
 set -e
 pip3.6 install --quiet --user pytest
 
-export INTROOT=/test/INTROOT
+mkdir $HOME/test_in_home
+cp -r /test $HOME/test_in_home
+ls $HOME/test_in_home
+
+export INTROOT=$HOME/test_in_home/INTROOT
 getTemplateForDirectory INTROOT $INTROOT
-source ~/.bashrc
-export ACS_CDB=/test/example/config
+source $HOME/.bashrc
+export ACS_CDB=$HOME/test_in_home/example/config
 env
 echo
 echo
 env | grep -i CDB
 
-cd /test/example
+cd $HOME/test_in_home/example
 rm -rf ./lib
 rm -rf ./object
 
-cd /test/example/src
+cd $HOME/test_in_home/example/src
 make all install
 
 acsStart
 acsStartContainer -py aragornContainer &
 
-cd /test/example
+cd $HOME/test_in_home/example
 python3.6 -m pytest --verbose -x -s ./test_it.py
